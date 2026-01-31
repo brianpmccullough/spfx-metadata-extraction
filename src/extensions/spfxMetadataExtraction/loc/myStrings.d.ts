61ecf4fb7d7a4d87e6f179025a6d6e3f5a38bb27
Brian P. McCullough <brianpmccullough@gmail.com>
2026-01-31 17:01:53 -0500

    initial setup

---
+++ src/extensions/spfxMetadataExtraction/loc/myStrings.d.ts
@@ -0,0 +1,9 @@
+declare interface ISpfxMetadataExtractionCommandSetStrings {
+  Command1: string;
+  Command2: string;
+}
+
+declare module 'SpfxMetadataExtractionCommandSetStrings' {
+  const strings: ISpfxMetadataExtractionCommandSetStrings;
+  export = strings;
+}
